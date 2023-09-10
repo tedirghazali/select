6fcc1eb6d57de4ade34e48bf5375823c3114cdf2
Tedir Ghazali <tedirghazali@gmail.com>
2023-09-10 15:17:29 +0700

    move to another env

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -40,19 +40,19 @@ const testSearch = (value: string) => {
   <div class="container mt-20px mb-20px">
     <h3>SelectBox (String)</h3>
     <SelectBox v-model="str" :options="arrayOfStrings" :size="5" @search="testSearch" />
-    <p>Your selected value is in string: {{ str }}</p>
+    <p>Your selected value is in a string: {{ str }}</p>
     <p><br/></p>
     <h3>SelectBox (Object)</h3>
     <SelectBox v-model="obj" :options="arrayOfObjects" :size="10" @search="testSearch" />
-    <p>Your selected value is in object: {{ obj }}</p>
+    <p>Your selected value is in an object: {{ obj }}</p>
     <p><br/></p>
     <h3>SelectBox (String value from array of objects)</h3>
     <SelectBox v-model="id" prop="value" datatype="string" dataprop="id" :options="arrayOfObjects" :size="10" @change="testChange" />
-    <p>Your selected value is in string from array of objects: {{ id }}</p>
+    <p>Your selected value is in a string from array of objects: {{ id }}</p>
     <p><br/></p>
     <h3>SelectBox (Number type of value from array of objects)</h3>
     <SelectBox v-model="num" prop="value" datatype="number" dataprop="id" :options="arrayOfObjects" :size="10" @change="testChange" />
-    <p>Your selected value is in number from array of objects: {{ num }}</p>
+    <p>Your selected value is in a number from array of objects: {{ num }}</p>
     <p><br/></p>
     <h3>SelectBox (Array of Strings)</h3>
     <SelectBox v-model="arrOfStr" :options="arrayOfStrings" :size="25" :up="true" />
@@ -65,28 +65,28 @@ const testSearch = (value: string) => {
     <p><br /><br /></p>
     <h3>ComboBox Autocomplete (String)</h3>
     <ComboBox v-model="comboStr" :options="arrayOfStrings" :size="5" @search="testSearch" />
-    <p>You choose the value in string: {{ comboStr }}</p>
+    <p>You choose the value in a string: {{ comboStr }}</p>
     <p><br/></p>
     <h3>ComboBox Autocomplete (Object)</h3>
     <ComboBox v-model="comboObj" :options="arrayOfObjects" :size="5" />
-    <p>You choose the value in object: {{ comboObj }}</p>
+    <p>You choose the value in an object: {{ comboObj }}</p>
     <p><br /></p>
     <h3>ComboBox Selection</h3>
     <ComboBox v-model="comboStr" :options="arrayOfStrings" :size="5" :select="true" />
-    <p>You choose the value in string: {{ comboStr }}</p>
+    <p>You choose the value in a string: {{ comboStr }}</p>
     
     <p><br /><br /></p>
     <h3>ListBox (String)</h3>
     <ListBox v-model="str" :options="arrayOfStrings" :size="5" @search="testSearch" />
-    <p>Your selected value is in string: {{ str }}</p>
+    <p>Your selected value is in a string: {{ str }}</p>
     <p><br/></p>
     <h3>ListBox (Object)</h3>
     <ListBox v-model="obj" :options="arrayOfObjects" :size="5" />
-    <p>Your selected value is in object: {{ obj }}</p>
+    <p>Your selected value is in an object: {{ obj }}</p>
     <p><br/></p>
     <h3>ListBox (String value from array of objects)</h3>
     <ListBox v-model="id" prop="value" datatype="string" dataprop="id" :options="arrayOfObjects" :size="5" @change="testChange" />
-    <p>Your selected value is in object: {{ id }}</p>
+    <p>Your selected value is in an object: {{ id }}</p>
     <p><br/></p>
     <h3>ListBox (Array of Strings)</h3>
     <ListBox v-model="arrOfStr" :options="arrayOfStrings" :size="5" />
@@ -99,7 +99,7 @@ const testSearch = (value: string) => {
     <p><br /><br /></p>
     <h3>TagBox (Only Array of Strings)</h3>
     <TagBox v-model="arrOfStr" :options="arrayOfStrings" :size="5" />
-    <p>Your selected value is in string: {{ arrOfStr }}</p>
+    <p>Your selected value is in a string: {{ arrOfStr }}</p>
     <!--<p><br/></p>
     <h3>TagBox (Objects)</h3>
     <TagBox v-model="arrOfObj" :options="arrayOfStrings" :size="5" />
